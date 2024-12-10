d2f3c1dfb70601e8326cd398e5f9be13ea622d78
Melek Demirhan <149537896+demirmelekhan@users.noreply.github.com>
2024-12-10 17:20:09 +0300

    yeni proje klasörü eklendi

---
+++ csharp-projeleri/Pratikler/datetime_math/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace datetime_math // Note: actual namespace depends on the project name.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine(DateTime.Now.Date);
+            Console.WriteLine(DateTime.Now.Day);
+            Console.WriteLine(DateTime.Now.Month);
+            Console.WriteLine(DateTime.Now.Year);
+            Console.WriteLine(DateTime.Now.Hour);
+            Console.WriteLine(DateTime.Now.Minute);
+            Console.WriteLine(DateTime.Now.Second);
+
+            Console.WriteLine(DateTime.Now.DayOfWeek);
+            Console.WriteLine(DateTime.Now.DayOfYear);
+
+            Console.WriteLine(DateTime.Now.ToLongDateString());
+            Console.WriteLine(DateTime.Now.ToShortDateString());
+            Console.WriteLine(DateTime.Now.ToLongTimeString());
+            Console.WriteLine(DateTime.Now.ToShortTimeString());
+
+            Console.WriteLine(DateTime.Now.AddDays(2));
+            Console.WriteLine(DateTime.Now.AddHours(2));
+            Console.WriteLine(DateTime.Now.AddSeconds(2));
+            Console.WriteLine(DateTime.Now.AddMonths(2));
+            Console.WriteLine(DateTime.Now.AddYears(2));
+            Console.WriteLine(DateTime.Now.AddMilliseconds(2));
+
+            //DAtetime Format
+            Console.WriteLine(DateTime.Now.ToString("dd")); //24
+            Console.WriteLine(DateTime.Now.ToString("ddd")); //Sun
+            Console.WriteLine(DateTime.Now.ToString("dddd")); //Sunday
+
+            Console.WriteLine(DateTime.Now.ToString("MM")); //10
+            Console.WriteLine(DateTime.Now.ToString("MMM")); //oct
+            Console.WriteLine(DateTime.Now.ToString("MMMM")); //october
+
+            Console.WriteLine(DateTime.Now.ToString("yy")); //24
+            Console.WriteLine(DateTime.Now.ToString("yyyy")); //2024
+
+            //MAth kütüphanesi
+
+            Console.WriteLine(Math.Abs(-23)); //23
+            Console.WriteLine(Math.Sin(10));
+            Console.WriteLine(Math.Cos(10));
+            Console.WriteLine(Math.Tan(10));
+
+            Console.WriteLine(Math.Ceiling(22.3)); //23
+            Console.WriteLine(Math.Round(23.3)); //22
+            Console.WriteLine(Math.Round(22.7)); //23
+            Console.WriteLine(Math.Floor(22.7)); //22
+
+            Console.WriteLine(Math.Max(2,6));
+            Console.WriteLine(Math.Min(2,6));
+
+            Console.WriteLine(Math.Pow(3,4));  //3^4 = 81
+            Console.WriteLine(Math.Sqrt(9)); // 9 karekökü 3
+
+            Console.WriteLine(Math.Log(9));
+            Console.WriteLine(Math.Exp(3));
+            Console.WriteLine(Math.Log10(10));
+        }
+    }
+}
