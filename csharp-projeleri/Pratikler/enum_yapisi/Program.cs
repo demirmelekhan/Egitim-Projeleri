c8914077febac3875df9ccdc7496212fd8cd5cf1
Melek Demirhan <149537896+demirmelekhan@users.noreply.github.com>
2024-12-18 08:11:45 +0300

    enum_yapisi proje klasörü eklendi

---
+++ csharp-projeleri/Pratikler/enum_yapisi/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace enum_yapisi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Gunler.Pazar);
+            Console.WriteLine((int)Gunler.Cumartesi);
+                
+            int sıcaklık =32;
+            if(sıcaklık<=(int)HavaDurumu.Normal)
+                Console.WriteLine("Dışarı çıkmak için soğuk");
+            else if(sıcaklık>=(int)HavaDurumu.Sıcak)
+                Console.WriteLine("Dışarıya çıkmak için çok sıcak");
+            else if(sıcaklık>=(int)HavaDurumu.Normal && sıcaklık<(int)HavaDurumu.ÇokSıcak)
+                Console.WriteLine("Hadii dışarıya çıkalım");
+        }
+
+
+        enum Gunler
+        {
+            Pazartesi,
+            Salı,
+            Çarşamba,
+            Perşembe,
+            Cuma,
+            Cumartesi,
+            Pazar
+        }
+        enum HavaDurumu
+        {
+            Soguk=5,
+            Normal=15,
+            Sıcak=25,
+            ÇokSıcak=30
+        }
+        
+    }
+}
